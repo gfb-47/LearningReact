d952adf97face6c0b1cfc0f48eebb183ea0e7d3c
gfb-47 <gabrieelfbrito@gmail.com>
2020-06-20 11:36:30 -0300

    api nutricao

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
 import React, { Component } from 'react';
 import './style.css';
 
+//https://sujeitoprogramador.com/rn-api/?api=posts
+
+
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      textoFrase: ''
+      nutri: []
     };
 
-    this.quebraBiscoito = this.quebraBiscoito.bind(this); 
-
-    this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.',
-      'O riso é a menor distância entre duas pessoas.',
-      'Deixe de lado as preocupações e seja feliz.',
-      'Realize o óbvio, pense no improvável e conquiste o impossível.',
-      'Acredite em milagres, mas não dependa deles.',
-      'A maior barreira para o sucesso é o medo do fracasso.'];
-
   }
 
-  quebraBiscoito(){
-    let state = this.state;
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
-    state.textoFrase = '"'+this.frases[numeroAleatorio]+'"';
-    this.setState(state);
+  componentDidMount() {
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+    fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        let state = this.state;
+        state.nutri = json;
+        this.setState(state);
+      })
   }
 
-  render() {
-    return(
-      <div className="container">
-        <img src={require('./assets/biscoito.png')} className="img" />
-        <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito} />
-    <h3 className="textoFrase">{this.state.textoFrase}</h3>
-      </div>
-    );
-  }
-}
-
-class Botao extends Component {
   render() {
     return (
-      <div>
-        <button onClick={this.props.acaoBtn} >{this.props.nome}</button>
+      <div className="container">
+        <header className>
+          <strong>React Nutri</strong>
+        </header>
+        {this.state.nutri.map((item) => {
+          return (
+            <article key={item.id} className="post">
+              <strong className="titulo"> {item.titulo} </strong>
+              <img src={item.capa} className="capa" />
+              <p className="subtitulo" >{item.subtitulo}</p>
+              <a className="botao" href="#">Acessar</a>
+            </article>
+
+          );
+        })}
       </div>
     );
   }
